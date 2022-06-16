e2da22ecbfa7f78534383007b95ae75fcf2d6534
DevOps Rob <robert.raybarnes@gmail.com>
2022-06-16 16:03:52 +0100

    adding boundary roles part to config script

--- modules/boundary/config/config.sh
+++ modules/boundary/config/config.sh
@@ -16,11 +16,39 @@ ACCOUNT_ID=$(boundary accounts create password -login-name=admin \
      -recovery-config="./config/recovery.hcl" \
      -format="json" | jq -r '.item.id')
 
-
-
 # create user
 USER_ID=$(boundary users create -name="admin" \
     -description="admin user for global scope" \
     -recovery-config="./config/recovery.hcl" \
     -format="json" | jq -r '.item.id')
+
+# Add account to user
+boundary users set-accounts \
+    -account=${ACCOUNT_ID} \
+    -id=${USER_ID} \
+    -version=1 \
+    -recovery-config="./config/recovery.hcl" \
+    -format="json" | jq 
+
+# Create admin role
+ROLE_ID=$(boundary roles create -name="admin" \
+    -scope-id="global" \
+    -description="admin role for global scope" \
+    -grant-scope-id="global" \
+    -recovery-config="./config/recovery.hcl" \
+    -format="json" | jq -r '.item.id')
+
+# Add grant strings
+boundary roles add-grants \
+    -grant="id=*;type=*;actions=*" \
+    -id=r_4fqtPAKYvD \
+    -recovery-config="./config/recovery.hcl" \
+    -format="json" | jq
+
+#Add user to role
+boundary roles add-principals \
+    -id=${ROLE_ID} \
+    -principal=${USER_ID} \
+    -recovery-config="./config/recovery.hcl" \
+    -format="json" | jq
 
